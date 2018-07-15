4fba43ca9860f561f8efadd13b6d5f5b0fc35357
Ryan Manchester <ryanm17@gmail.com>
2018-07-15 18:38:40 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,10 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
+  post '/recipes' do
+    @recipe = Recipe.create(:name=> params[:name], :ingredients=> params[:ingredients], :cook_time=> params[:cook_time])
+  end
+
   get '/recipes/:id' do
     @recipe = Recipe.find_by(:id=> params[:id])
     erb :show
